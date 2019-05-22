90776bb84fa44c35b7ece96aca60a0fb7b2b37c0
mihaitmf <mihai.tmf@gmail.com>
2019-05-22 16:42:56 +0300

    Remove git-files.sh provisioner

--- .dev/provisioning/shell/bootstrap.sh
+++ .dev/provisioning/shell/bootstrap.sh
@@ -8,8 +8,5 @@ GIT_USER_EMAIL=${4}
 echo "Running git-config.sh"
 ${SHELL_SCRIPTS_DIR}/git-config.sh ${PROJECT_DIR} ${GIT_USER_NAME} ${GIT_USER_EMAIL}
 
-echo "Running git-files.sh"
-${SHELL_SCRIPTS_DIR}/git-files.sh ${PROJECT_DIR}
-
 echo "Running set-working-dir.sh"
 ${SHELL_SCRIPTS_DIR}/set-working-dir.sh ${PROJECT_DIR}
--- .dev/provisioning/shell/git-config.sh
+++ .dev/provisioning/shell/git-config.sh
@@ -7,5 +7,11 @@ GIT_USER_EMAIL=${3}
 apt update >/dev/null
 apt install -y git >/dev/null
 
+# Check installation succeeded
+if ! type git >/dev/null 2>&1; then
+    echo "ERROR installing git"
+    exit 1
+fi
+
 # Config git user
 (cd ${PROJECT_DIR}; git config user.name "${GIT_USER_NAME}" && git config user.email ${GIT_USER_EMAIL})
